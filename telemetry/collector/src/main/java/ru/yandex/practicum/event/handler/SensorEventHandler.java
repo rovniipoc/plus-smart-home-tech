930ac4b1232f7ed15afab6fa0ed2eae177b90fca
Estet <7vt-blest@rambler.ru>
2025-03-10 22:10:27 +0400

    Перевести на gRPC SensorEvent

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/event/handler/SensorEventHandler.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.event.handler;
+
+import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
+
+public interface SensorEventHandler {
+
+    SensorEventProto.PayloadCase getMessageType();
+
+    void handle(SensorEventProto event);
+
+}
