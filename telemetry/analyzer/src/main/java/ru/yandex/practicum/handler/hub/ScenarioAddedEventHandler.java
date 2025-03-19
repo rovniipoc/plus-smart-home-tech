3c42d23d33f69451654ef982866b7812e1099e38
Estet <7vt-blest@rambler.ru>
2025-03-19 22:49:14 +0400

    Добавить обработчики событий хаба

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/handler/hub/ScenarioAddedEventHandler.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.handler.hub;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.ScenarioAddedEventAvro;
+import ru.yandex.practicum.model.Scenario;
+import ru.yandex.practicum.repository.ScenarioRepository;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ScenarioAddedEventHandler implements HubEventHandler {
+
+    private final ScenarioRepository scenarioRepository;
+
+    @Override
+    public String getEventType() {
+        return ScenarioAddedEventAvro.getClassSchema().getName();
+    }
+
+    @Transactional
+    @Override
+    public void handle(HubEventAvro event) {
+
+        ScenarioAddedEventAvro scenarioEvent = (ScenarioAddedEventAvro) event.getPayload();
+        Scenario scenario = new Scenario();
+        scenario.setHubId(event.getHubId());
+        scenario.setName(scenario.getName());
+
+        scenarioRepository.save(scenario);
+        log.info("В БД сохранен scenario: {}", scenario);
+    }
+}
