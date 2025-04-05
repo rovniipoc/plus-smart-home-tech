b792c3ad4118ec5f79270bb9a99029f37b35c3d2
Estet <7vt-blest@rambler.ru>
2025-04-05 21:52:23 +0400

    Создать dto NewProductRequest

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/dto/NewProductRequest.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.util.UUID;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class NewProductRequest {
+
+    UUID productId;
+
+    @NotBlank
+    private String productName;
+
+    @NotBlank
+    private String description;
+
+    private String imageSrc;
+
+    @NotNull
+    private QuantityState quantityState;
+
+    @NotNull
+    private ProductState productState;
+
+    @NotNull
+    private ProductCategory productCategory;
+
+    @Min(value = 1, message = "Price must be at least 1")
+    private Double price;
+}
