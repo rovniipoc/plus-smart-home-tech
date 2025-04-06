6a28a7e8d88efe30e43559afae809a74b14bd8af
Estet <7vt-blest@rambler.ru>
2025-04-06 21:20:35 +0400

    Изменить Long на UUID

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/repository/ProductRepository.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/repository/ProductRepository.java
@@ -6,8 +6,9 @@ import ru.yandex.practicum.dto.ProductCategory;
 import ru.yandex.practicum.model.Product;
 
 import java.util.List;
+import java.util.UUID;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     List<Product> findAllByProductCategory(ProductCategory category, Pageable pageable);
 
