0d319f21b9c2b109d88e77d95217ec711344fbad
Estet <7vt-blest@rambler.ru>
2025-04-06 21:01:48 +0400

    Добавить главный класс

---
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/ShoppingStoreApp.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingStoreApp
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingStoreApp.class, args);
+    }
+}
