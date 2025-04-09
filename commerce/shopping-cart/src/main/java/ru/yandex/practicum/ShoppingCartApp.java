176537d9e12144c473eed467e5da3fc178410458
Estet <7vt-blest@rambler.ru>
2025-04-09 17:54:42 +0400

    Исправить по чекстайлу

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/ShoppingCartApp.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/ShoppingCartApp.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoppingCartApp
-{
+public class ShoppingCartApp {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApp.class, args);
     }
--- commerce/shopping-store/src/main/java/ru/yandex/practicum/ShoppingStoreApp.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/ShoppingStoreApp.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoppingStoreApp
-{
+public class ShoppingStoreApp {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingStoreApp.class, args);
     }
