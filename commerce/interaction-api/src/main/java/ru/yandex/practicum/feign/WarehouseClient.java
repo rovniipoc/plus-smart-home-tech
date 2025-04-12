f00339e56be726ac605305e23a85e3a12107a10f
Estet <7vt-blest@rambler.ru>
2025-04-13 00:13:56 +0400

    Добавить circuit breaker

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/feign/WarehouseClient.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/feign/WarehouseClient.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import ru.yandex.practicum.dto.BookedProductsDto;
 import ru.yandex.practicum.dto.ShoppingCartDto;
 
-@FeignClient(name = "warehouse")
+@FeignClient(
+        name = "warehouse",
+        fallback = WarehouseClientFallback.class
+)
 public interface WarehouseClient {
     @PostMapping("/api/v1/warehouse/check")
     BookedProductsDto checkProductQuantityEnoughForShoppingCart(@RequestBody ShoppingCartDto cart) throws FeignException;
